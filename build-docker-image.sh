6db350257a0bbf808e494b0bd9aac51a28b024d1
Justice Adams <adamsjal@uni.edu>
2018-06-04 23:02:10 -0700

    cleaned docker files

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -2,3 +2,6 @@
 
 # Build docker image
 docker build -t android .
+
+
+read -p "Docker image done building and ready for use! Press enter to continue"
--- buildapk.sh
+++ buildapk.sh
@@ -2,7 +2,9 @@
 
 # Build debug APK using new docker container. Make sure you run build-docker-image.sh first
 # The Kotlin compiler has some issues when the gradle dameon runs in parralel, so these flags are necessary
-docker run -it --rm -v "$PWD":/application android ./gradlew assembleDebug -Dorg.gradle.daemon=false -Dorg.gradle.parallel=false -Dkotlin.incremental=false
+docker run -it --rm -v /$PWD:/application android ./gradlew assembleDebug -Dorg.gradle.daemon=false -Dorg.gradle.parallel=false -Dkotlin.incremental=false
 
 # Move the apk file to the base directory
 mv ./app/build/outputs/apk/debug/app-debug.apk .
+
+read -p "APK done building! Press enter to continue"
