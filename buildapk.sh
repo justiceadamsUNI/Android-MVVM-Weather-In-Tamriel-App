59486d7168e9551edd84c4051c5ee2c25c298f7e
Justice Adams <adamsjal@uni.edu>
2018-06-04 22:42:16 -0700

    seperated image building and apk building into seperate build scripts

--- buildapk.sh
+++ buildapk.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Build docker image
-docker build -t android .
-
-# Build debug APK using new docker container
+# Build debug APK using new docker container. Make sure you run build-docker-image.sh first
 # The Kotlin compiler has some issues when the gradle dameon runs in parralel, so these flags are necessary
 docker run -it --rm -v "$PWD":/application android ./gradlew assembleDebug -Dorg.gradle.daemon=false -Dorg.gradle.parallel=false -Dkotlin.incremental=false
 
